db317779ce7315f0fa5eb34496755c493237ffc3
LoptrSir <91218015+LoptrSir@users.noreply.github.com>
2023-06-19 13:33:14 -0600

    html btn class/id JS decimal, equal, clear create

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-let display;
-let num1;
-let operand;
-let num2;
+let display = 0;
+let num1 = "0";
+let operand = undefined; //wen value assigned re-enable decimal click
+let num2 = "0";
 
+//basic math functions
 function add(num1, num2) {
   display = num1 + num2;
-  console.log(display);
+  console.log("+", display);
   return display;
 }
 
 function subtract(num1, num2) {
   display = num1 - num2;
-  console.log(display);
+  console.log("-", display);
   return display;
 }
 
 function multiply(num1, num2) {
   display = num1 * num2;
-  console.log(display);
+  console.log("*", display);
   return display;
 }
 
@@ -27,13 +28,61 @@ function divide(num1, num2) {
     return alert("cannot divide by zero");
   } else {
     display = num1 / num2;
-    console.log(display);
+    console.log("/", display);
     return display;
   }
 }
 
+// need code to turn off event listener to prevent multiple events of . in single num.
+//Then it needs to be turned off once num1 changes to num2 or upon clear
+function decimal(operand) {
+  if (operand == undefined) {
+    num1 += ".";
+    //click disable
+  } else {
+    num2 += ".";
+    //click disable
+  }
+}
+
+// do i need a decimal click re-enable here?  I believe so.
+function clear(num1, num2) {
+  display = 0;
+  num1 = undefined;
+  operand = undefined;
+  num2 = undefined;
+  console.log("clear", display, num1, operand, num2);
+}
+// switch case to trigger equals button press.
+// This needs to be wrapped in the equals button call/function otherwise it will trigger once read by comp.
+function equals() {
+  // re-enable decimal listener? or just use on clear?
+  switch (operand) {
+    case "+":
+      add();
+      break;
+    case "-":
+      subtract();
+      break;
+    case "*":
+      multiply();
+      break;
+    case "/":
+      divide();
+      break;
+    default:
+      console.log("switch", "Houston we have a problem!");
+  }
+}
+//event listener
+function clickHandler() {}
+let btn = document.getElementsByClassName(".buttons");
+btn.addEventListener("click", clickHandler);
+
+//Console function tests
 add(3, 2);
 subtract(3, 2);
 multiply(3, 2);
 divide(2, 0);
 divide(4, 2);
+clear();
